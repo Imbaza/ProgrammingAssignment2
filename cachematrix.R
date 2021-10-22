2aeadd9e3933069e8baea173abfb1a2776b555f2
Imbaza <92580479+Imbaza@users.noreply.github.com>
2021-10-22 13:02:30 +0530

    Revert "Update cachematrix.R"
    This reverts commit aded3057f388d4894c9d090dad4c1453023dc806.

--- cachematrix.R
+++ cachematrix.R
@@ -1,29 +1,15 @@
-## Inverse Function
+## Put comments here that give an overall description of what your
+## functions do
 
-## helps reduce cache time
+## Write a short comment describing this function
+
 makeCacheMatrix <- function(x = matrix()) {
-  inv <- NULL
-  set <- function(y) {
-    x <<- y
-    inv <<- NULL
-  }
-  get <- function() {x}
-  setInverse <- function(Inverse) inv <<- {Inverse}
-  getInverse <- function() {inv}
-  list(set = set, get = get,
-       setInverse = setInverse,
-       getInverse = getInverse)
+
 }
 
-## Cache Solve returns Inverse
+
+## Write a short comment describing this function
+
 cacheSolve <- function(x, ...) {
-  inv <- x$getInverse()
-  if(!is.null(inv)) {
-    message("getting cached data")
-    return(inv)
-  }
-  mat <- x$get()
-  inv <- solve(mat, ...)
-  x$setInverse(inv)
-  inv
-} 
+        ## Return a matrix that is the inverse of 'x'
+}
